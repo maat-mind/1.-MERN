bdced85b81c5f3c016a7d49730387bcfadbe0cb2
Oscar Bernal <odbg2013@gmail.com>
2022-09-10 22:50:53 -0500

    update 1. MERN

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 1337
 
+const mongoose = require('mongoose')
+const connString = `mongodb+srv://maatmind:${ password }>@cluster0.lleyzz9.mongodb.net/test`
+mongoose.connect(connString)
+
 app.listen(port, () => {
   console.log('hi there')
 })
