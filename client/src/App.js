35a9b0aeae95c53b378dcdf3b3c2446dc508607f
Oscar Bernal <odbg2013@gmail.com>
2022-09-11 13:16:37 -0500

    update 1. MERN

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,22 @@ import { useState } from 'react'
 
 function App() {
 
-  const [userList, setUserList] = useState([])
+  const [userList, setUserList] = useState([
+    { id: 1, name: "Pedro", age: 20, username: "pedro123" },
+  ])
 
   return (
     <>
       <span className="usersDisplay">
-
+        { userList.map((user) => {
+          return (
+            <>
+              <h1>Name: { user.name }</h1>
+              <h1>Age: { user.age }</h1>
+              <h1>Username: { user.username }</h1>
+            </>
+          )
+        }) }
       </span>
     </>
   )
