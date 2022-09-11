c8c175c30256cb1204c8ae1fd8e7bbe6d3a7d0a7
Oscar Bernal <odbg2013@gmail.com>
2022-09-10 22:55:53 -0500

    update 1. MERN

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 1337
 
 const mongoose = require('mongoose')
-const connString = `mongodb+srv://maatmind:${ password }>@cluster0.lleyzz9.mongodb.net/test`
+const connString = `mongodb+srv://maatmind:${ process.env.DB_PASSWORD }@cluster0.lleyzz9.mongodb.net/test`
+console.log(connString)
 mongoose.connect(connString)
 
 app.listen(port, () => {
