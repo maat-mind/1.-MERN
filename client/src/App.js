aafb9349a0c9bde6db2aab0ba2961b1e0c2c7ad6
Oscar Bernal <odbg2013@gmail.com>
2022-09-11 13:13:01 -0500

    update 1. MERN

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  const [userList, setUserList] = useState([])
+
+  return (
+    <>
+      <span className="usersDisplay">
+
+      </span>
+    </>
+  )
+}
+
+export default App
