fd11f4e110700db1778a1eadf8f2a4c1ae761c49
Oscar Bernal <odbg2013@gmail.com>
2022-09-10 23:02:38 -0500

    update 1. MERN

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const app = express()
 const port = 1337
 
 const mongoose = require('mongoose')
-const connString = `mongodb+srv://maatmind:${ process.env.DB_PASSWORD }@cluster0.lleyzz9.mongodb.net/test`
-console.log(connString)
+
+const connString = `mongodb+srv://maatmind:${ process.env.DB_PASSWORD }@cluster0.lleyzz9.mongodb.net/mern-tutorial?retryWrites=true&w=majority`
+
 mongoose.connect(connString)
 
 app.listen(port, () => {
