06c317cc05e073e08bdb080fc3020ca0707f4bec
Oscar Bernal <odbg2013@gmail.com>
2022-09-10 23:18:14 -0500

    update 1. MERN

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 require('dotenv').config()
 
+// EXPRESS
 const express = require('express')
 const app = express()
 const port = 1337
 
+// MONGODB
 const mongoose = require('mongoose')
+const UserModel = require('./models/Users')
 
 const connString = `mongodb+srv://maatmind:${ process.env.DB_PASSWORD }@cluster0.lleyzz9.mongodb.net/mern-tutorial?retryWrites=true&w=majority`
 
 mongoose.connect(connString)
+
+// HTTP REQUESTS
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, data) => {
+    if (err) console.error(err)
+
+    res.json(data)
+  })
+})
+
 
 app.listen(port, () => {
   console.log('hi there')
